a84f6096156a8a76fbcffda1b1a95db012b06002
Filis Futsarov <filisfutsarov@gmail.com>
2025-03-24 20:02:22 +0100

    update name.sh

--- name.sh
+++ name.sh
@@ -3,10 +3,10 @@
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
 
-compose="$DIR/docker/docker-compose.yml"
+compose="$DIR/docker/setup/docker-compose.yml"
 
 certs="$DIR/docker/setup/containers/nginx/install_certs_in_host.sh"
 
 domain="$1"
 
-sed -i "s|\framework.dev|${domain}|g" $compose
+sed -i "s|\domain|framework.dev|g" $compose
