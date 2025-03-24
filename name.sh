0b1a9ef0fe3e3d90c6bec48289c5139c3938febc
Filis Futsarov <filisko@users.noreply.github.com>
2025-03-24 19:37:26 +0000

    Update name.sh

--- name.sh
+++ name.sh
@@ -2,7 +2,6 @@
 
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
-
 compose="$DIR/docker/setup/docker-compose.yml"
 certs="$DIR/docker/setup/containers/nginx/install_certs_in_host.sh"
 
